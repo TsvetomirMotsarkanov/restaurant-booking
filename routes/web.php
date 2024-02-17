739d2a6923f258f5775f1d01036f473e24501e1c
tsvetomir-motsarkanov <tsvetomir.motsarkanov@gmail.com>
2024-02-18 01:34:10 +0200

    remove the auth guard and return dashboard view by default

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->name('home');
+// ->middleware(['auth', 'verified']);
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
