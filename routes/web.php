e338f967f8f5c24dcc16b124de140d0a2a35b81b
tsvetomir-motsarkanov <tsvetomir.motsarkanov@gmail.com>
2024-03-02 17:30:32 +0200

    remove react

--- routes/web.php
+++ routes/web.php
@@ -17,18 +17,9 @@ use Inertia\Inertia;
 */
 
 // Route::get('/', function () {
-//     return view('dashboard');
+//     return 'test';
 // })->name('home');
 
 
 Route::get('/', [RestaurantController::class, 'index'])->name('home');
 Route::get('/restaurants/{restaurant}', [RestaurantController::class, 'view']);
-// ->middleware(['auth', 'verified']);
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__ . '/auth.php';
