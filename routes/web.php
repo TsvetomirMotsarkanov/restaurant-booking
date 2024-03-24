495a090bb078c38512757bbbb6a92cd7d0b78a4d
tsvetomir-motsarkanov <tsvetomir.motsarkanov@gmail.com>
2024-03-24 13:51:21 +0200

    remove all unneeded files

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\BookingController;
-use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\RestaurantController;
 use Illuminate\Support\Facades\Route;
 
@@ -21,15 +20,3 @@ Route::get('/restaurants/{restaurant}', [RestaurantController::class, 'view']);
 
 Route::get('/booking/{restaurant}', [BookingController::class, 'show']);
 Route::post('/booking', [BookingController::class, 'store'])->name('booking.create');
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__ . '/auth.php';
