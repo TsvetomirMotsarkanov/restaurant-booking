893e4bed6ba88a2c8e60cd0a06e0c4b1ae7f3902
tsvetomir-motsarkanov <tsvetomir.motsarkanov@gmail.com>
2024-03-24 16:37:12 +0200

    update header favicon and layout

--- resources/views/restaurant/partials/additional-info.blade.php
+++ resources/views/restaurant/partials/additional-info.blade.php
@@ -2,83 +2,85 @@
     class="lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
     <div class="lg:pr-4">
         <div class="max-w-xl text-base leading-7 text-gray-700 lg:max-w-lg">
-            <ul role="list" class="mt-8 mb-8 space-y-8 text-gray-600">
-                <x-additional-info name="name" :value="$restaurant->raiting">
+
+            <h2 class="mt-0 text-2xl font-bold tracking-tight text-gray-900">Additional Information:</h2>
+            <ul role="list" class="grid grid-cols-2 gap-x-16 items-baseline space-y-8 text-gray-600">
+                <x-additional-info name="Raiting" :value="$restaurant->raiting">
                     <path fill-rule="evenodd"
                         d="M5.5 17a4.5 4.5 0 01-1.44-8.765 4.5 4.5 0 018.302-3.046 3.5 3.5 0 014.504 4.272A4 4 0 0115 17H5.5zm3.75-2.75a.75.75 0 001.5 0V9.66l1.95 2.1a.75.75 0 101.1-1.02l-3.25-3.5a.75.75 0 00-1.1 0l-3.25 3.5a.75.75 0 101.1 1.02l1.95-2.1v4.59z"
                         clip-rule="evenodd" />
                 </x-additional-info>
-                <x-additional-info name="name" :value="$restaurant->additional_info_area">
+                <x-additional-info name="Area" :value="$restaurant->additional_info_area">
                     <path fill-rule="evenodd"
                         d="M5.5 17a4.5 4.5 0 01-1.44-8.765 4.5 4.5 0 018.302-3.046 3.5 3.5 0 014.504 4.272A4 4 0 0115 17H5.5zm3.75-2.75a.75.75 0 001.5 0V9.66l1.95 2.1a.75.75 0 101.1-1.02l-3.25-3.5a.75.75 0 00-1.1 0l-3.25 3.5a.75.75 0 101.1 1.02l1.95-2.1v4.59z"
                         clip-rule="evenodd" />
                 </x-additional-info>
-                <x-additional-info name="name" :value="$restaurant->additional_info_location">
+                <x-additional-info name="Location" :value="$restaurant->additional_info_location">
                     <path fill-rule="evenodd"
                         d="M5.5 17a4.5 4.5 0 01-1.44-8.765 4.5 4.5 0 018.302-3.046 3.5 3.5 0 014.504 4.272A4 4 0 0115 17H5.5zm3.75-2.75a.75.75 0 001.5 0V9.66l1.95 2.1a.75.75 0 101.1-1.02l-3.25-3.5a.75.75 0 00-1.1 0l-3.25 3.5a.75.75 0 101.1 1.02l1.95-2.1v4.59z"
                         clip-rule="evenodd" />
                 </x-additional-info>
-                <x-additional-info name="name" :value="$restaurant->additional_info_hours_of_operation">
+                <x-additional-info name="Hours of operation" :value="$restaurant->additional_info_hours_of_operation">
                     <path fill-rule="evenodd"
                         d="M5.5 17a4.5 4.5 0 01-1.44-8.765 4.5 4.5 0 018.302-3.046 3.5 3.5 0 014.504 4.272A4 4 0 0115 17H5.5zm3.75-2.75a.75.75 0 001.5 0V9.66l1.95 2.1a.75.75 0 101.1-1.02l-3.25-3.5a.75.75 0 00-1.1 0l-3.25 3.5a.75.75 0 101.1 1.02l1.95-2.1v4.59z"
                         clip-rule="evenodd" />
                 </x-additional-info>
-                <x-additional-info name="name" :value="$restaurant->additional_info_price">
+                <x-additional-info name="Prices" :value="$restaurant->additional_info_price">
                     <path fill-rule="evenodd"
                         d="M5.5 17a4.5 4.5 0 01-1.44-8.765 4.5 4.5 0 018.302-3.046 3.5 3.5 0 014.504 4.272A4 4 0 0115 17H5.5zm3.75-2.75a.75.75 0 001.5 0V9.66l1.95 2.1a.75.75 0 101.1-1.02l-3.25-3.5a.75.75 0 00-1.1 0l-3.25 3.5a.75.75 0 101.1 1.02l1.95-2.1v4.59z"
                         clip-rule="evenodd" />
                 </x-additional-info>
-                <x-additional-info name="name" :value="$restaurant->additional_info_cuisines">
+                <x-additional-info name="Cuisines" :value="$restaurant->additional_info_cuisines">
                     <path fill-rule="evenodd"
                         d="M5.5 17a4.5 4.5 0 01-1.44-8.765 4.5 4.5 0 018.302-3.046 3.5 3.5 0 014.504 4.272A4 4 0 0115 17H5.5zm3.75-2.75a.75.75 0 001.5 0V9.66l1.95 2.1a.75.75 0 101.1-1.02l-3.25-3.5a.75.75 0 00-1.1 0l-3.25 3.5a.75.75 0 101.1 1.02l1.95-2.1v4.59z"
                         clip-rule="evenodd" />
                 </x-additional-info>
-                <x-additional-info name="name" :value="$restaurant->additional_info_dining_style">
+                <x-additional-info name="Dining Style" :value="$restaurant->additional_info_dining_style">
                     <path fill-rule="evenodd"
                         d="M5.5 17a4.5 4.5 0 01-1.44-8.765 4.5 4.5 0 018.302-3.046 3.5 3.5 0 014.504 4.272A4 4 0 0115 17H5.5zm3.75-2.75a.75.75 0 001.5 0V9.66l1.95 2.1a.75.75 0 101.1-1.02l-3.25-3.5a.75.75 0 00-1.1 0l-3.25 3.5a.75.75 0 101.1 1.02l1.95-2.1v4.59z"
                         clip-rule="evenodd" />
                 </x-additional-info>
-                <x-additional-info name="name" :value="$restaurant->additional_dress_code">
+                <x-additional-info name="Dress code" :value="$restaurant->additional_dress_code">
                     <path fill-rule="evenodd"
                         d="M5.5 17a4.5 4.5 0 01-1.44-8.765 4.5 4.5 0 018.302-3.046 3.5 3.5 0 014.504 4.272A4 4 0 0115 17H5.5zm3.75-2.75a.75.75 0 001.5 0V9.66l1.95 2.1a.75.75 0 101.1-1.02l-3.25-3.5a.75.75 0 00-1.1 0l-3.25 3.5a.75.75 0 101.1 1.02l1.95-2.1v4.59z"
                         clip-rule="evenodd" />
                 </x-additional-info>
-                <x-additional-info name="name" :value="$restaurant->additional_parking_details">
+                <x-additional-info name="Parking" :value="$restaurant->additional_parking_details">
                     <path fill-rule="evenodd"
                         d="M5.5 17a4.5 4.5 0 01-1.44-8.765 4.5 4.5 0 018.302-3.046 3.5 3.5 0 014.504 4.272A4 4 0 0115 17H5.5zm3.75-2.75a.75.75 0 001.5 0V9.66l1.95 2.1a.75.75 0 101.1-1.02l-3.25-3.5a.75.75 0 00-1.1 0l-3.25 3.5a.75.75 0 101.1 1.02l1.95-2.1v4.59z"
                         clip-rule="evenodd" />
                 </x-additional-info>
-                <x-additional-info name="name" :value="$restaurant->additional_payment_options">
+                <x-additional-info name="Payment Options" :value="$restaurant->additional_payment_options">
                     <path fill-rule="evenodd"
                         d="M5.5 17a4.5 4.5 0 01-1.44-8.765 4.5 4.5 0 018.302-3.046 3.5 3.5 0 014.504 4.272A4 4 0 0115 17H5.5zm3.75-2.75a.75.75 0 001.5 0V9.66l1.95 2.1a.75.75 0 101.1-1.02l-3.25-3.5a.75.75 0 00-1.1 0l-3.25 3.5a.75.75 0 101.1 1.02l1.95-2.1v4.59z"
                         clip-rule="evenodd" />
                 </x-additional-info>
-                <x-additional-info name="name" :value="$restaurant->additional_executive_chef">
+                <x-additional-info name="Executive Chef" :value="$restaurant->additional_executive_chef">
                     <path fill-rule="evenodd"
                         d="M5.5 17a4.5 4.5 0 01-1.44-8.765 4.5 4.5 0 018.302-3.046 3.5 3.5 0 014.504 4.272A4 4 0 0115 17H5.5zm3.75-2.75a.75.75 0 001.5 0V9.66l1.95 2.1a.75.75 0 101.1-1.02l-3.25-3.5a.75.75 0 00-1.1 0l-3.25 3.5a.75.75 0 101.1 1.02l1.95-2.1v4.59z"
                         clip-rule="evenodd" />
                 </x-additional-info>
-                <x-additional-info name="name" :value="$restaurant->additional_phone_number">
+                <x-additional-info name="Phone number" :value="$restaurant->additional_phone_number">
                     <path fill-rule="evenodd"
                         d="M5.5 17a4.5 4.5 0 01-1.44-8.765 4.5 4.5 0 018.302-3.046 3.5 3.5 0 014.504 4.272A4 4 0 0115 17H5.5zm3.75-2.75a.75.75 0 001.5 0V9.66l1.95 2.1a.75.75 0 101.1-1.02l-3.25-3.5a.75.75 0 00-1.1 0l-3.25 3.5a.75.75 0 101.1 1.02l1.95-2.1v4.59z"
                         clip-rule="evenodd" />
                 </x-additional-info>
-                <x-additional-info name="name" :value="$restaurant->additional_website">
+                <x-additional-info name="Website" :value="$restaurant->additional_website">
                     <path fill-rule="evenodd"
                         d="M5.5 17a4.5 4.5 0 01-1.44-8.765 4.5 4.5 0 018.302-3.046 3.5 3.5 0 014.504 4.272A4 4 0 0115 17H5.5zm3.75-2.75a.75.75 0 001.5 0V9.66l1.95 2.1a.75.75 0 101.1-1.02l-3.25-3.5a.75.75 0 00-1.1 0l-3.25 3.5a.75.75 0 101.1 1.02l1.95-2.1v4.59z"
                         clip-rule="evenodd" />
                 </x-additional-info>
-                <x-additional-info name="name" :value="$restaurant->additional_menu">
+                <x-additional-info name="Menu" :value="$restaurant->additional_menu">
                     <path fill-rule="evenodd"
                         d="M5.5 17a4.5 4.5 0 01-1.44-8.765 4.5 4.5 0 018.302-3.046 3.5 3.5 0 014.504 4.272A4 4 0 0115 17H5.5zm3.75-2.75a.75.75 0 001.5 0V9.66l1.95 2.1a.75.75 0 101.1-1.02l-3.25-3.5a.75.75 0 00-1.1 0l-3.25 3.5a.75.75 0 101.1 1.02l1.95-2.1v4.59z"
                         clip-rule="evenodd" />
                 </x-additional-info>
-                <x-additional-info name="name" :value="$restaurant->additional_private_party_facilities">
+                <x-additional-info name="Private party" :value="$restaurant->additional_private_party_facilities">
                     <path fill-rule="evenodd"
                         d="M5.5 17a4.5 4.5 0 01-1.44-8.765 4.5 4.5 0 018.302-3.046 3.5 3.5 0 014.504 4.272A4 4 0 0115 17H5.5zm3.75-2.75a.75.75 0 001.5 0V9.66l1.95 2.1a.75.75 0 101.1-1.02l-3.25-3.5a.75.75 0 00-1.1 0l-3.25 3.5a.75.75 0 101.1 1.02l1.95-2.1v4.59z"
                         clip-rule="evenodd" />
                 </x-additional-info>
-                <x-additional-info name="name" :value="$restaurant->additional_smoking_area">
+                <x-additional-info name="Smoking area" :value="$restaurant->additional_smoking_area">
                     <path fill-rule="evenodd"
                         d="M5.5 17a4.5 4.5 0 01-1.44-8.765 4.5 4.5 0 018.302-3.046 3.5 3.5 0 014.504 4.272A4 4 0 0115 17H5.5zm3.75-2.75a.75.75 0 001.5 0V9.66l1.95 2.1a.75.75 0 101.1-1.02l-3.25-3.5a.75.75 0 00-1.1 0l-3.25 3.5a.75.75 0 101.1 1.02l1.95-2.1v4.59z"
                         clip-rule="evenodd" />
